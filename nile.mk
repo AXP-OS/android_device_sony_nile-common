6fa8e208ab92a2fdf56d259be2ab2ae2e0289169
steadfasterX <steadfasterX@users.noreply.github.com>
2025-03-28 20:04:25 +0100

    mk: enable AXP.OS

--- nile.mk
+++ nile.mk
@@ -468,3 +468,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096
+
+# enable AXP.OS vendor                                                                                                                                                                                                                 
+$(call inherit-product, vendor/axp/config/common.mk)
